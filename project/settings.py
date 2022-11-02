c6103566a98bf5eb5a748c0ca843730d932cf052
amicuswat <amicuswat@gmail.com>
2022-11-02 22:56:00 +0300

    add API to create and get balance data

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'manage_bots.apps.ManageBotsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from manage_bots.views import BalanceAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/balancelist/', BalanceAPIView.as_view())
 ]
