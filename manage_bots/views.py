ed689ee2d7d3bf565c9fbe8c458f9faccb07df78
Sergey <Sergey@localhost.localdomain>
2022-11-07 17:06:39 +0300

    Changed API views for using with router

--- manage_bots/views.py
+++ manage_bots/views.py
@@ -1,13 +1,18 @@
-from rest_framework import generics
-from django.shortcuts import render
-from .models import Balance
-from .serializers import BalanceSerializer
+from rest_framework import viewsets
+from .models import Balance, TelegramOrganisation, TelegramUser
+from .serializers import BalanceSerializer, OrganisationSerializer, MemberSerializer
 
 # Create your views here.
-class BalanceAPIView(generics.ListCreateAPIView):
+class BalanceAPIView(viewsets.ModelViewSet):
     queryset = Balance.objects.all()
     serializer_class = BalanceSerializer
 
 
+class OrganisationAPIView(viewsets.ModelViewSet):
+    queryset = TelegramOrganisation.objects.all()
+    serializer_class = OrganisationSerializer
 
 
+class MemberAPIView(viewsets.ModelViewSet):
+    queryset = TelegramUser.objects.all()
+    serializer_class = MemberSerializer
