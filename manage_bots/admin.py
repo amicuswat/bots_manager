b4c469366d7fad8af71d889e31dd78ed04297c4b
Sergey <Sergey@localhost.localdomain>
2022-11-13 16:31:32 +0300

    Add model for telegram group

--- manage_bots/admin.py
+++ manage_bots/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Balance, TelegramUser, Bot
+from .models import Balance, TelegramUser, Bot, TelegramGroup
 
 # Register your models here.
-admin.site.register([Balance, TelegramUser, Bot])
+admin.site.register([Balance, TelegramUser, TelegramGroup, Bot])
